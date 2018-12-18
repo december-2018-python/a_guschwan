9addec6bc26bc1fe63514e9858245363a442a77c
Andy Guschwan <aguschwan@gmail.com>
2018-12-17 18:28:42 -0600

    ninja gold

--- 02-flask/02-flask/09-ninja_gold/server.py
+++ 02-flask/02-flask/09-ninja_gold/server.py
@@ -2,39 +2,42 @@ from flask import Flask, render_template, request, redirect, session, Markup
 import random, datetime
 app = Flask(__name__)
 app.secret_key = "afjasfkja87"
-@app.route('/', methods=["get","post"])
 
+@app.route('/', methods=["get","post"])
 def index():
-    add = 0
-    if add not in session:
+    try:
+        session['num']
+    except:
         session['num'] = 0
-    elif session['add'] > 0:
-        tmp = session['message'] 
-        session['message'] = "Earned " + str(session['add']) + " golds from the " + session['place'] + "  " + "(" + str(datetime.datetime.now()) + ")"
-        session['message'] = session['message'] + "\n" + tmp  
-    else:
-        tmp = session['message'] 
-        session['message'] = "Entered a casino and lost " + str(session['add']) + " golds... Ouch." + "  " + "(" + str(datetime.datetime.now()) + ")"
-        session['message'] = session['message'] + "\n" + tmp
     return render_template("index.html" )
 
 @app.route('/process_money', methods=["get","post"])
 def process_money():
-    num = 0
+    add = 0
+    
+    try:
+        session['message']
+    except:
+        session['message'] =[]
+
     if request.form['building'] == 'farm':
-        num = random.randrange(10,20)
+        add = random.randrange(10,20)
         session['place'] = 'farm'
     elif request.form['building'] == 'house':
-        num = random.randrange(2,5)
+        add = random.randrange(2,5)
         session['place'] = 'house'
     elif request.form['building'] == 'cave':
-        num = random.randrange(5,10)
+        add = random.randrange(5,10)
         session['place'] = 'cave'
     else:
-        num = random.randrange(-50,50)
+        add = random.randrange(-50,50)
         session['place'] = 'casino'
-    session['add'] = num
-    session['num'] = session['num'] + num   
+    session['num'] += add
+
+    if add > 0:
+        session['message'].insert(0, "<p class='green'>" + "Earned " + str(add) + " golds from the " + session['place'] + "  " + "(" + str(datetime.datetime.now()) + ")" + "</p>")
+    else: 
+        session['message'].insert(0, "<p class='red'>" + "Entered a casino and lost " + str(add) + " golds... Ouch." + "  " + "(" + str(datetime.datetime.now()) + ")" + "</p>") 
     return redirect("/")
 
 if __name__=="__main__":
